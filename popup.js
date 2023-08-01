8ec3e595fce53d00c9809fc486e4db9abaca446d
Rohboam Amin <rohbam.99@gmail.com>
2023-08-01 16:30:46 +0100

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,3 +1,24 @@
+// Store the encryption key here
+let encryptionKey;
+
+// Function to generate the encryption key (if not already generated) and return it
+async function getEncryptionKey() {
+  if (!encryptionKey) {
+    // If the encryption key is not already generated, generate it and save it to encryptionKey
+    encryptionKey = await crypto.subtle.generateKey(
+      {
+        name: 'AES-GCM',
+        length: 256,
+      },
+      true,
+      ['encrypt', 'decrypt']
+    );
+  }
+
+  return encryptionKey;
+}
+
+
 // Function to get stored passwords from Chrome storage
 function getStoredPasswords(callback) {
     chrome.storage.sync.get(['passwords'], function(result) {
@@ -37,27 +58,89 @@ function generatePassword() {
   return password;
 }
 
-  // Function to handle login and display stored passwords
-  function handleLogin() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-  
+// Function to handle login and display stored passwords
+async function handleLogin() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  if (!username || !password) {
+    alert('Please enter a valid username and password.');
+    return;
+  }
+
+  try {
+    // Get the encryption key
+    const encryptionKey = await getEncryptionKey();
+
     // Implement account authentication here (e.g., with the Python backend)
-  
-    // On successful login, show stored passwords
-    getStoredPasswords(function(passwords) {
-      const passwordList = document.getElementById('passwordList');
-      passwordList.innerHTML = '';
-      for (const [site, encryptedPassword] of Object.entries(passwords)) {
-        // Implement decryption here (e.g., with the Python backend)
-        // Decrypted password is stored in decryptedPassword variable
-  
-        const li = document.createElement('li');
-        li.textContent = `${site}: ${decryptedPassword}`;
-        passwordList.appendChild(li);
+    authenticateUser(username, password, async function(isAuthenticated) {
+      if (isAuthenticated) {
+        // On successful login, show stored passwords
+        getStoredPasswords(async function(passwords) {
+          const passwordList = document.getElementById('passwordList');
+          passwordList.innerHTML = '';
+          for (const [site, storedPasswordObject] of Object.entries(passwords)) {
+            try {
+              // Decrypted password is stored in decryptedPassword variable
+              const decryptedPassword = await decryptPassword(
+                JSON.parse(storedPasswordObject.encryptedPassword),
+                encryptionKey
+              );
+
+              const li = document.createElement('li');
+              li.textContent = `${site}: ${decryptedPassword}`;
+              passwordList.appendChild(li);
+            } catch (error) {
+              console.error('Error decrypting password:', error);
+              alert('Failed to decrypt password. Please try again.');
+            }
+          }
+        });
+      } else {
+        alert('Invalid username or password. Please try again.');
       }
     });
+  } catch (error) {
+    console.error('Error during login:', error);
+    alert('Login failed. Please try again.');
   }
+}
+
+
+// Function to authenticate the user
+function authenticateUser(username, password, callback) {
+  getAllStoredPasswords(function(passwords) {
+    const storedPassword = passwords[username];
+    console.log(storedPassword)
+    if (storedPassword) {
+      // For demonstration purposes, let's assume the stored password is a JSON-serialized object
+      const storedPasswordObject = JSON.parse(storedPassword.encryptedPassword);
+
+      // Implement decryption here (e.g., with the Python backend)
+      // For demonstration, let's assume decryptedPassword contains the actual password
+      const decryptedPassword = decryptPassword(storedPasswordObject);
+
+      if (password === decryptedPassword) {
+        callback(true); // Authentication success
+      } else {
+        callback(false); // Authentication failed
+      }
+    } else {
+      callback(false); // User not found (Authentication failed)
+    }
+  });
+}
+
+
+function getAllStoredPasswords(callback) {
+  chrome.storage.sync.get(['passwords'], function(result) {
+    const passwords = result.passwords || {};
+    console.log(passwords)
+    callback(passwords);
+  });
+}
+
+
   
 // Function to handle password generation and display the generated password
 function handleGeneratePassword() {
@@ -97,9 +180,37 @@ async function encryptPassword(password, encryptionKey) {
   return encryptedPassword;
 }
 
+
+
+
+// Function to decrypt the password using AES-GCM
+async function decryptPassword(storedPasswordObject, encryptionKey) {
+  try {
+    // Import the encryption key
+    const importedEncryptionKey = await crypto.subtle.importKey(
+      'jwk',
+      encryptionKey,
+      { name: 'AES-GCM', length: 256 },
+      false,
+      ['decrypt']
+    );
+
+    const iv = storedPasswordObject.iv;
+    const data = storedPasswordObject.data;
+
+    const decryptedData = await crypto.subtle.decrypt({ name: 'AES-GCM', iv }, importedEncryptionKey, data);
+    const decoder = new TextDecoder();
+    const decryptedPassword = decoder.decode(decryptedData);
+
+    return decryptedPassword;
+  } catch (error) {
+    console.error('Error decrypting password:', error);
+    throw new Error('Failed to decrypt password. Please try again.');
+  }
+}
+
 // Function to save passwords to Chrome storage
 async function savePassword(site, username, password) {
-  
   try {
     const encryptionKey = await crypto.subtle.generateKey(
       {
@@ -112,15 +223,24 @@ async function savePassword(site, username, password) {
 
     const encryptedPassword = await encryptPassword(password, encryptionKey);
 
+    // Convert the encryptedPassword object to a JSON-serializable string
+    const serializedEncryptedPassword = JSON.stringify(encryptedPassword);
+
     chrome.storage.sync.get(['passwords'], function(result) {
       const passwords = result.passwords || {};
       passwords[site] = {
         username: username,
-        encryptedPassword: encryptedPassword,
+        encryptedPassword: serializedEncryptedPassword, // Save the serialized string
       };
       chrome.storage.sync.set({ passwords: passwords }, function() {
+      if (chrome.runtime.lastError) {
+        console.error('Error saving password:', chrome.runtime.lastError);
+        alert('Failed to save password. Please try again.');
+      } else {
         alert('Password saved successfully!');
-      });
+        console.log(passwords);
+      }
+    });
     });
   } catch (error) {
     console.error('Error saving password:', error);
@@ -130,91 +250,48 @@ async function savePassword(site, username, password) {
 
 
 
+
 // Function to handle account creation
 function handleAccountCreation() {
-  // // alert('Please enter a valid username and password.');
 
-  // const passwordManagerContainer = document.querySelector('.container');
-  // const accountCreationContainer = document.getElementById('accountCreationContainer');
+  // Get the elements
+  const newUsername = document.getElementById('newUsername').value;
+  const newPassword = document.getElementById('newPassword').value;
 
-  // passwordManagerContainer.style.display = 'none';
-  // accountCreationContainer.style.display = 'block';
 
-  
-  // const newUsername = document.getElementById('newUsername').value;
-  // const newPassword = document.getElementById('newPassword').value;
 
-  // // Get the elements
-  // const newUsernameElement = document.getElementById('newUsername');
-  // const newPasswordElement = document.getElementById('newPassword');
-
-  // // Assign values to the elements
-  // newUsernameElement.value = 'test';
-  // newPasswordElement.value = '1234';
-
-  // console.log('newUsername:', newUsername);
-  // console.log('newPassword:', newPassword);
-
-  // if (!newUsername || !newPassword) {
-  //   alert('Please enter a valid username and password.');
-  //   return;
-  // }
-
-  // // Save the username and password to Chrome storage
-  // savePassword(newUsername, newUsername, newPassword); // Use the username as the site for simplicity
-
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-
-  if (!username || !password) {
-  alert('Please enter a valid username and password.');
-  return;
+  if (!newUsername || !newPassword) {
+    alert('Please enter a valid username and password.');
+    return;
   }
 
-    // Save the username and password to Chrome storage
-  savePassword(username, username, password); // Use the username as the site for simplicity
+  // Save the username and password to Chrome storage
+  savePassword(newUsername, newUsername, newPassword); // Use the username as the site for simplicity
+
+
   
 }
 
 
 // Function to open the account creation page in the existing window
 function openAccountCreationWindow() {
-  // alert('Please enter a valid username and password.');
-
   const passwordManagerContainer = document.querySelector('.container');
   const accountCreationContainer = document.getElementById('accountCreationContainer');
 
   passwordManagerContainer.style.display = 'none';
   accountCreationContainer.style.display = 'block';
 
-  // const newUsername = document.getElementById('newUsername').value;
-  // const newPassword = document.getElementById('newPassword').value;
 
-  // if (!newUsername || !newPassword) {
-  //   alert('Please enter a valid username and password.');
-  //   return;
-  // }
-
-  // alert('Please enter a valid username and password.');
-  // // Save the username and password to Chrome storage
-  // savePassword(newUsername, newUsername, newPassword); // Use the username as the site for simplicity
-}
-
-// Function to go back to the main password manager UI
-function goBackToPasswordManager() {
-  const passwordManagerContainer = document.querySelector('.container');
-  const accountCreationContainer = document.getElementById('accountCreationContainer');
-
-  passwordManagerContainer.style.display = 'block';
-  accountCreationContainer.style.display = 'none';
 }
 
 
-document.getElementById('loginButton').addEventListener('click', handleAccountCreation);
-document.getElementById('generatePasswordButton').addEventListener('click', handleGeneratePassword);
+
+document.getElementById('loginButton').addEventListener('click', handleLogin);
+document.getElementById('generatePasswordButton').addEventListener('click', getAllStoredPasswords);
 // Add event listener for the "Create Account" button
-document.getElementById('createAccountButton').addEventListener('click', openAccountCreationWindow);
-// Add event listener for the "Back" button
-document.getElementById('backButton').addEventListener('click', goBackToPasswordManager);
+// document.getElementById('createAccountButton').addEventListener('click', openAccountCreationWindow);
 
 document.getElementById('submitAccountButton').addEventListener('click', handleAccountCreation);
+
+
+
